fd5c574e715c884adfcfe40753f18611269bbb74
ash-fitzgibbon <107602601+ash-fitzgibbon@users.noreply.github.com>
2022-09-21 19:54:03 +1200

    idk

---
+++ examples/post-1/assets/tilemaps/volcano.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="volcano" tilewidth="32" tileheight="32" tilecount="52" columns="4">
+ <image source="C:/Users/ashfi/Downloads/volcano.png" width="128" height="416"/>
+</tileset>

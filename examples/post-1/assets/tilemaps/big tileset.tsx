aea6b1e29b78556b185664d92bcb6768d8e004b3
ash-fitzgibbon <107602601+ash-fitzgibbon@users.noreply.github.com>
2022-09-21 23:04:32 +1200

    added new map

---
+++ examples/post-1/assets/tilemaps/big tileset.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="big tileset" tilewidth="32" tileheight="32" tilecount="5208" columns="8">
+ <image source="C:/Users/ashfi/Downloads/big tileset.png" width="256" height="20832"/>
+ <tile id="1575">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8" y="4" width="16.5104" height="25"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ examples/post-1/assets/tilemaps/pokemon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="pokemon" tilewidth="32" tileheight="32" tilecount="420" columns="15">
+ <image source="C:/Users/ashfi/Downloads/pokemon.png" width="496" height="912"/>
+</tileset>
